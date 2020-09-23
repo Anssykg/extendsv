a07309a397f16ba14d1c1eb1007270784b00bea9
1099815904@qq.com <1099815904@qq.com>
2020-09-23 12:03:24 +0800

    添加推送

---
+++ src/library/HttpHandle.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @Created by 666.
+ * @User: Anssykg
+ * @Date: 2020/7/22 0022
+ * @Time: 下午 22:56
+ */
+
+namespace Extendsv\library;
+
+class HttpHandle
+{
+    /**
+     * @http_send_curl  发起推送
+     * @Date: 2020/9/23 11:59
+     * @param $url
+     * @param bool $params [arr]
+     * @param int $isPost
+     * @param int $https
+     * @return bool|mixed [description]
+     */
+    function http_send_curl($url, $params = false, $isPost = 0, $https = 0)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($https) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
+        }
+        if ($isPost) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+            curl_setopt($ch, CURLOPT_URL, $url);
+        } else {
+            if ($params) {
+                if (is_array($params)) {
+                    $params = http_build_query($params);
+                }
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
+        $response = curl_exec($ch);
+        if ($response === FALSE) {
+            return false;
+        }
+        curl_close($ch);
+        $response = str_replace('﻿', '', $response);
+        $responseResult = json_decode($response, true);
+        return $responseResult;
+    }
+}
