7d5999a8ea805f1eb2e6276969f373094bee1235
1099815904@qq.com <1099815904@qq.com>
2020-10-10 17:43:49 +0800

    添加http，get,sslget,post

--- src/library/HttpHandle.php
+++ src/library/HttpHandle.php
@@ -11,6 +11,7 @@ namespace Extendsv\library;
 class HttpHandle
 {
     /**
+     * 该方法有的服务平台不支持
      * @http_send_curl  发起推送
      * @Date: 2020/9/23 11:59
      * @param $url
@@ -25,7 +26,7 @@ class HttpHandle
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         if ($https) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
@@ -53,5 +54,76 @@ class HttpHandle
         $response = str_replace('﻿', '', $response);
         $responseResult = json_decode($response, true);
         return $responseResult;
+    }
+
+    /**
+     * @getHttp  发起推送
+     * @param $url
+     * @return bool|string [description]
+     */
+    public function getHttp($url)
+    {
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        curl_close($curl);
+        return $data;
+
+    }
+
+    /**
+     * @getSSLHttp  发起推送
+     * @param $url
+     * @return bool|string [description]
+     */
+    public function getSSLHttp($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // https请求 不验证证书和hosts
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        $data = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if ($httpCode != 200) {
+            $data = "https connect timeout";
+        }
+        curl_close($curl);
+        return $data;
+    }
+
+    /**
+     * @post_curls  发起推送
+     * @param $url
+     * @param $post
+     * @return bool|string [description]
+     */
+    public function post_curls($url, $post)
+    {
+        $curl = curl_init(); // 启动一个CURL会话
+        $HTTP_USER_AGENT = $_SERVER['HTTP_USER_AGENT'] ?? 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36';
+        curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
+        curl_setopt($curl, CURLOPT_USERAGENT, $HTTP_USER_AGENT); // 模拟用户使用的浏览器
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+        curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post); // Post提交的数据包
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+        curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+        $res = curl_exec($curl); // 执行操作
+        if (curl_errno($curl)) {
+            echo 'Errno' . curl_error($curl);//捕抓异常
+        }
+        curl_close($curl); // 关闭CURL会话
+        return $res; // 返回数据，json格式
+
     }
 }
