fdde18cccdec7251dfb5a541edd9e43a3ce59cdd
袁开国 <Anssykg@example.com>
2022-07-28 16:41:35 +0800

    日期问题

--- src/Utils/TimeSystem.php
+++ src/Utils/TimeSystem.php
@@ -127,4 +127,49 @@ class TimeSystem
         ];
         return compact('now_day_arr', 'yst_day_arr', 'this_week_arr', 'within_week_arr', 'this_month_arr', 'within_month_arr', 'yst_month_arr', 'tis_year_arr', 'yst_month_time_str');
     }
+
+
+    /**
+     * 获取n月后的今天
+     * @param $n
+     * @return false|string
+     */
+    public static function getNextMonthTimeStr($n)
+    {
+        $nd = date("d");//今天多少号
+        $nt = date("t");//当月最多多少号
+        $lt = date("t", strtotime($n . " month"));//n月后最多多少号
+        $newxtMonthTimeStr = date('Y-m-d H:i:s', strtotime($n . " month"));
+        if ($nt > $lt) {//当月号数比n月后号数大
+            if ($nd > $lt) {//今天号数比n月后号数大
+                //取n月后下个月的第一天
+                $newxtMonthTimeStr = date('Y-m-1 H:i:s', strtotime($n + 1 . " month"));
+            }
+        }
+        return $newxtMonthTimeStr;
+    }
+
+    /**
+     * 获取n月前的今天
+     * @param $n
+     * @return false|string
+     */
+    public static function getPreMonthTimeStr($n)
+    {
+        $nd = date("d");//今天多少号
+        $nt = date("t");//当月最多多少号
+        $lt = date("t", strtotime("-" . $n . " month"));//n月前最多多少号
+        $preMonthTimeStr = date('Y-m-d H:i:s', strtotime("-" . $n . " month"));
+        if ($nt > $lt) {//当月号数比n月后号数大
+            if ($nd > $lt) {//今天号数比n月后号数大
+                $preMonthTimeStr = date('Y-m-1 00:00:00', strtotime("-" . ($n - 1) . " month"));
+            }
+        }
+        if ($nt < $lt) {
+            if ($nt == $nd) {
+                $preMonthTimeStr = date('Y-m-d H:i:s', strtotime("-1 day", strtotime("-" . $n . " day")));
+            }
+        }
+        return $preMonthTimeStr;
+    }
 }
